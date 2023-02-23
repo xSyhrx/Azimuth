3c9ddd7ecbf6670c20ae6930a97976705d87fd82
Rhys Koutts <s220297@students.aie.edu.au>
2023-02-24 10:24:48 +1100

    Project Linking Setup

--- Azimuth-Test/Program.cs
+++ Azimuth-Test/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿namespace Azimuth_Test
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			
+		}
+	}
+}
